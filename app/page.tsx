b0ad23487c2ef58ab11af65a9f67ebc371595a73
1965nj <jafaroun1998@gmail.com>
2024-04-28 14:40:40 +0100

    final modul

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-    <h1>Pour acceder a la page chat, taper http://localhost:3000/personal-info puis cliquer sur chat,
+    <h1>Pour acceder a la page chat, taper "http://localhost:3000/personal-info" puis cliquer sur chat,
       Il est a noter qu'ici l'application tourne sur le port 3000
     </h1>
     </>
