854dcd8583ee8a74ce0a69c9c055ec6fb5df1bf1
1965nj <jafaroun1998@gmail.com>
2024-04-28 14:08:54 +0100

    indication

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <>
-    <h1>Starter code</h1>
+    <h1>Pour acceder a la page chat, taper http://localhost:3000/personal-info puis cliquer sur chat,
+      Il est a noter qu'ici l'application tourne sur le port 3000
+    </h1>
     </>
   );
 }
